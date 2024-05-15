88e094bcd31d3b3faa1d1a4b07383fb3e458939a
Simon <simonyakushev@gmail.com>
2024-05-15 14:38:37 +0300

    first commit

---
+++ helper/funcs.py
@@ -0,0 +1,17 @@
+from icecream import ic
+def addition2 (num1,num2):
+    ic(num1+num2)
+def addition3 (num1,num2,num3):
+    ic(num1+num2+num3)
+def mult (num1,num2,):
+     ic (num1*num2,)
+def divide(num1,num2):
+    ic(num1/num2)
+def printName ():
+    ic("OBEY")  
+def isbigger(num1, num2):
+    if num1 > num2:
+        ic(num1)
+    else:
+        ic(num2)
+         
---
+++ main.py
@@ -0,0 +1,10 @@
+
+from helper.funcs import addition2,addition3,mult,divide,printName,isbigger
+
+if __name__=="__main__":
+    addition2(2,3)
+    addition3(2,4,6)
+    mult(2,7)
+    divide(10,5)
+    printName()
+    isbigger(69,55)
